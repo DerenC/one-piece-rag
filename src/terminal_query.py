0cf60b32dc505f48e73e477979df33297a9e6095
DerenC <chengderen953@gmail.com>
2026-05-24 00:00:46 +0800

    Show sources & improve print output format for terminal_query

--- src/terminal_query.py
+++ src/terminal_query.py
@@ -1,3 +1,9 @@
 from query import rag_query
 
-print(rag_query(input("Ask me any question about the anime One Piece\n")))
+response, sources = rag_query(input("Ask me any question about the anime One Piece\n"))
+
+print(f"\nResponse:\n{response}\n")
+
+print("\nSources:")
+for i, source in enumerate(sources):
+    print(f"{'---' if i != 0 else ''}\n{source}")
