7f7fb8432480bdf2efabdac8d3ffa30aa2d4227e
DerenC <chengderen953@gmail.com>
2026-05-23 21:23:59 +0800

    Add HF_TOKEN while loading HF embedding model

--- src/emb_model.py
+++ src/emb_model.py
@@ -1,6 +1,19 @@
+from dotenv import load_dotenv
 from sentence_transformers import SentenceTransformer
+import os
 
-emb_model = SentenceTransformer('all-MiniLM-L6-v2')  # 384-dim embeddings
+load_dotenv()
 
-def encode(texts: list[str]):
-    return emb_model.encode(texts)
+minilm_emb_model = SentenceTransformer('all-MiniLM-L6-v2', token=os.environ.get("HF_TOKEN"))  # 384-dim embeddings
+
+def minilm_encode(texts: list[str]):
+    return minilm_emb_model.encode(texts)
+
+# TODO
+# mpnet_emb_model = SentenceTransformer('microsoft/mpnet-base', token=os.environ.get("HF_TOKEN"))  # 384-dim embeddings
+
+# def mpnet_encode(texts: list[str]) -> list[list[float]]:
+#     return mpnet_emb_model.encode(texts)
+
+
+encode = minilm_encode
