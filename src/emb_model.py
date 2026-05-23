b1eea7ab0dfa6dbcde5c988949ac5be029b9d6d8
DerenC <chengderen953@gmail.com>
2026-05-23 21:44:33 +0800

    Add commented out BAAI/bge-large-en-v1.5 emb model to explore in future

--- src/emb_model.py
+++ src/emb_model.py
@@ -4,6 +4,7 @@ import os
 
 load_dotenv()
 
+# minilm_emb_model = SentenceTransformer('BAAI/bge-large-en-v1.5', token=os.environ.get("HF_TOKEN"))  # 384-dim embeddings
 minilm_emb_model = SentenceTransformer('all-MiniLM-L6-v2', token=os.environ.get("HF_TOKEN"))  # 384-dim embeddings
 
 def minilm_encode(texts: list[str]):
@@ -12,8 +13,9 @@ def minilm_encode(texts: list[str]):
 # TODO
 # mpnet_emb_model = SentenceTransformer('microsoft/mpnet-base', token=os.environ.get("HF_TOKEN"))  # 384-dim embeddings
 
-# def mpnet_encode(texts: list[str]) -> list[list[float]]:
+# def mpnet_encode(texts: list[str]):
 #     return mpnet_emb_model.encode(texts)
 
+# encode = mpnet_encode
 
 encode = minilm_encode
